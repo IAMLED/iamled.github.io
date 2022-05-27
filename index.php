ceb00e056393e62fb1588b0834404f561f52a273
IAMLED <106379039+IAMLED@users.noreply.github.com>
2022-05-27 21:57:56 +0100

    added css and js files

--- index.php
+++ index.php
@@ -5,10 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>STUORG</title>
+    
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor">
+    <link rel="stylesheet" href="assets/css/custom.css">
 </head>
 <body>
-    <?php  
-        echo "Welcome To STUORG" 
-    ?>
+    <div class="container">
+        <div class="content">
+            <h1>Welcome to Stuorg</h1>
+            <div class="login_form">
+                <form action="" class="form-group">
+                    <input type="submet" value="SUBMIT" class="btn btn-secondary">
+                </form>
+            </div>
+        </div>
+        
+    </div>
+
+        <!-- JavaScript Bundle with Popper -->
+    <script src="assets\js\jquery-3.6.0.js"></script>    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"></script>
 </body>
 </html>
