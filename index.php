aec4b353f91bcfcf3d2a576687d4a81ef6e034cc
IAMLED <106379039+IAMLED@users.noreply.github.com>
2022-05-27 23:25:21 +0100

    added new fonts

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
         
         <!-- CSS only -->
         <link rel="stylesheet" href="assets\css\bootstrap.min.css">
+        <link rel="stylesheet" href="assets\font-awesome-4.7.0\css\font-awesome.min.css">
         <link rel="stylesheet" href="assets/css/custom.css">
     </head>
     <body>
