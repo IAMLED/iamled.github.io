5d8cec52333064307a2371eba2a4b0f48d64bcae
IAMLED <106379039+IAMLED@users.noreply.github.com>
2022-05-27 23:08:59 +0100

    designed the index page and added font awesome

--- index.php
+++ index.php
@@ -1,30 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>STUORG</title>
-    
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor">
-    <link rel="stylesheet" href="assets/css/custom.css">
-</head>
-<body>
-    <div class="container">
-        <div class="content">
-            <h1>Welcome to Stuorg</h1>
-            <div class="login_form">
-                <form action="" class="form-group">
-                    <input type="submet" value="SUBMIT" class="btn btn-secondary">
-                </form>
-            </div>
-        </div>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>STUORG</title>
         
-    </div>
+        <!-- CSS only -->
+        <link rel="stylesheet" href="assets\css\bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/custom.css">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Welcome to Stuorg</h1>
+        </div>
+        <div class="container-sm content card " style="width: 32rem; background-color: #22415f">
+                
+            <h2 id="login_label">Login</h2>
+           
+            <div class="login_form">
+                <form>
+                    <div class="mb-3">
+                        <input type="email" class="form-control" id="login_email" aria-describedby="emailHelp" placeholder="Username/Email">
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" id="login_password" placeholder="Password">
+                    </div>
+                    <button type="submit" class="btn btn-light ">Submit</button>
+                </form>
+                
+            </div>
 
-        <!-- JavaScript Bundle with Popper -->
-    <script src="assets\js\jquery-3.6.0.js"></script>    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"></script>
-</body>
+            <div class="login_options">
+                    <div class="row ">
+                        <div class="col align-items-start">
+                            Forgotten Password?
+                        </div>
+                        <div class="col align-items-end">
+                            New Student? Signup.
+                        </div>
+                    </div>
+            </div>
+            
+
+
+        </div>
+            
+        
+
+            <!-- JavaScript Bundle with Popper -->
+        <script src="assets\js\jquery-3.6.0.js"></script> 
+        <script src="assets\js\bootstrap.bundle.min.js"></script>   
+    </body>
 </html>
