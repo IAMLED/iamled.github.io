987c5160a8c9751fcc14c0b32455533a31c87810
IAMLED <106379039+IAMLED@users.noreply.github.com>
2022-05-28 00:39:10 +0100

    added some styling to the index

--- index.php
+++ index.php
@@ -10,43 +10,45 @@
         <link rel="stylesheet" href="assets\css\bootstrap.min.css">
         <link rel="stylesheet" href="assets\font-awesome-4.7.0\css\font-awesome.min.css">
         <link rel="stylesheet" href="assets/css/custom.css">
+        <link rel="favicon" href="">
     </head>
     <body>
-        <div class="container">
-            <h1>Welcome to Stuorg</h1>
-        </div>
-        <div class="container-sm content card " style="width: 32rem; background-color: #22415f">
-                
-            <h2 id="login_label">Login</h2>
-           
-            <div class="login_form">
-                <form>
-                    <div class="mb-3">
-                        <input type="email" class="form-control" id="login_email" aria-describedby="emailHelp" placeholder="Username/Email">
-                    </div>
-                    <div class="mb-3">
-                        <input type="password" class="form-control" id="login_password" placeholder="Password">
-                    </div>
-                    <button type="submit" class="btn btn-light ">Submit</button>
-                </form>
-                
+        <div class="covers container-fluid">
+            <div class="containers">
+                <h1 id="welcome">Welcome to Stuorg</h1>
             </div>
+            <div class="container-sm content card " style="width: 32rem; background-color: #22415f">
+                    
+                <h2 id="login_label">Login</h2>
+            
+                <div class="login_form">
+                    <form>
+                        <div class="mb-3">
+                            <input type="email" class="form-control" id="login_email" aria-describedby="emailHelp" placeholder="Username/Email">
+                        </div>
+                        <div class="mb-3">
+                            <input type="password" class="form-control" id="login_password" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-light ">Submit</button>
+                    </form>
+                    
+                </div>
 
-            <div class="login_options">
-                    <div class="row ">
-                        <div class="col align-items-start">
-                            Forgotten Password?
+                <div class="login_options">
+                        <div class="row ">
+                            <div class="col align-items-start text-white-50">
+                               <a href=""> Forgotten Password? </a>
+                            </div>
+                            <div class="col align-items-end">
+                               <a href=""> New Student? Signup. </a>
+                            </div>
                         </div>
-                        <div class="col align-items-end">
-                            New Student? Signup.
-                        </div>
-                    </div>
+                </div>
+                
+
+
             </div>
-            
-
-
-        </div>
-            
+        </div>    
         
 
             <!-- JavaScript Bundle with Popper -->
